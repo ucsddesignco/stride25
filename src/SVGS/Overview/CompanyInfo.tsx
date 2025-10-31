2a1c69d979693c7dd4725b8303994604969aca14
wangserene <sew004@ucsd.edu>
2025-10-30 17:26:55 -0700

    pr take two

--- src/SVGS/Overview/CompanyInfo.tsx
+++ src/SVGS/Overview/CompanyInfo.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+
 import type { SVGProps } from "react";
 const SvgCompanyInfo = (props: SVGProps<SVGSVGElement>) => (
   <svg
--- src/SVGS/Overview/OneToOne.tsx
+++ src/SVGS/Overview/OneToOne.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+
 import type { SVGProps } from "react";
 const SvgOneToOne = (props: SVGProps<SVGSVGElement>) => (
   <svg
